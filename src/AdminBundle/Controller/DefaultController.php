ccb2b30eb4f776af746c7f56f6cb454da41867f5
wenhao <854854321@qq.com>
2019-03-05 16:12:32 +0800

    [MOD] add linfo Bundle

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Linfo\Linfo;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $linfo = new \Linfo\Linfo();
+        $parser = $linfo->getParser();
+        dump($parser->getRam());die;
         return $this->render('@Admin/Default/index.html.twig');
     }
 }
